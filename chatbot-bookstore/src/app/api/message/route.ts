b342402ae8c7636a3705106881b35520fdd974c7
Soran Raof <soranraof@hotmail.com>
2023-06-18 12:13:58 +0100

    ..

--- chatbot-bookstore/src/app/api/message/route.ts
+++ chatbot-bookstore/src/app/api/message/route.ts
@@ -1,5 +1,9 @@
 import { chatbotPrompt } from "@/helpers/constants/chatbot-prompts";
-import { ChatGPTMessage } from "@/lib/openai-steam";
+import {
+  ChatGPTMessage,
+  OpenAIStream,
+  OpenAIStreamPayload,
+} from "@/lib/openai-steam";
 import { MessageArraySchema } from "@/lib/validators/messages";
 
 export async function POST(req: Request) {
@@ -7,13 +11,31 @@ export async function POST(req: Request) {
 
   const parsedMessages = MessageArraySchema.parse(messages);
 
-  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => ({
-    role: message.isUserMessage ? "user" : "system",
-    content: message.text,
-  }));
+  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => {
+    return {
+      role: message.isUserMessage ? "user" : "system",
+      content: message.text,
+    };
+  });
 
   outboundMessages.unshift({
     role: "system",
     content: chatbotPrompt,
   });
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+
+  return new Response(stream);
 }
